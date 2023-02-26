ec1a3e21479e01fe22be9ed9c8ce4b08c0e71c6c
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-26 20:47:01 +0300

    new const

---
+++ src/components/Game.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import {SafeAreaView, StyleSheet} from "react-native";
+import {Colors} from "../styles/colors";
+import {PanGestureHandler} from "react-native-gesture-handler";
+import {GestureEventType} from "../types/types";
+
+const SNAKE_INITIAL_POSITION = [{ x: 5, y: 5 }];
+const FOOD_INITIAL_POSITION = { x: 5, y: 20 };
+const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax: 63 };
+const MOVE_INTERVAL = 50;
+const SCORE_INCREMENT = 10;
+
+export default function Game(): JSX.Element {
+  const handleGesture = (event: GestureEventType) => {
+    // console.log(event.nativeEvent)
+    const {translationX, translationY} = event.nativeEvent
+
+    if (Math.abs(translationX) > Math.abs(translationY)) {
+      if (translationX > 0) {
+        //  moving right
+      } else {
+        //  moving left
+      }
+    } else {
+      if (translationY > 0) {
+        //  down
+      } else {
+        //  up
+      }
+    }
+  }
+
+  return (
+    <PanGestureHandler onGestureEvent={handleGesture}>
+      <SafeAreaView style={styles.container}>
+
+      </SafeAreaView>
+    </PanGestureHandler>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.primary,
+  }
+})
